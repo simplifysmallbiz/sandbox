73fdd6729294ad95ec315a98fb07e45f0debd45f
Andrenellin <andrenellin@gmail.com>
2023-01-20 07:00:29 -0700

    Added gravityflow_timeline_note_add

---
+++ gravityflow_timeline_note_add.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * When a note is added, save the note to a field in the associated entry
+ *
+ */
+
+add_filter('gravityflow_timeline_note_add', 'user_input_audit', 10, 5);
+function user_input_audit($note, $entry_id, $user_id, $user_name, $step)
+{
+// Log the note so that you can see the variable to reference
+    bs($note);
+
+    // for a specific step id corresponding to your user input step
+    if ($step->get_id() == 8) {
+
+        $entry = $step->get_entry();
+        $note_to_add = $note[1]; // Will update this line after viewing the log for bs($note)
+        // $note_field_in_entry = rgar($entry, 20); // Field ID of multiline text field in form
+
+        $input_id = 20; // The field id in the form entry you want to add the note to
+        $result = GFAPI::update_entry_field($entry_id, $input_id, $note_to_add);
+    }
+    return $note;
+}
